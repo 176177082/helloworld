8f7c2ceb4c1060bde1f36a2dc2042f3759a0bd01
176177082 <176177082@qq.com>
2019-03-18 10:22:35 +0800

    同步 异步  回调

---
+++ ybcallbacktb.js
@@ -0,0 +1,18 @@
+// 函数表达式，匿名函数
+var func2=function(){
+
+
+    //console.log("333");
+    console.log("func2");
+}
+var func1=function(callback){
+    //do something.
+    console.log("func1");
+    for (var i =0; i<5;i++){
+        console.log(i);
+    }
+    setTimeout(function(){console.log('func1')},1000);
+    callback();
+}
+
+func1(func2);
---
+++ ybcallbackyb.js
@@ -0,0 +1,20 @@
+// 函数表达式，匿名函数
+var func2=function(){
+
+    setTimeout(function(){console.log('func2')},1000);
+
+
+    //console.log("333");
+    console.log("func2");
+}
+var func1=function(callback){
+    //do something.
+    console.log("func1");
+    for (var i =0; i<5;i++){
+        console.log(i);
+    }
+    setTimeout(function(){console.log('func1')},1000);
+    callback();
+}
+
+func1(func2);
